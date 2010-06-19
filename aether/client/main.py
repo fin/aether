62901a5b342fe7add34f153ed1776126ca933916
Markus Hametner <fin+github@xbhd.org>
2010-06-19 19:03:40 +0200

    announce and lookup via zeroconf

--- aether/client/main.py
+++ aether/client/main.py
@@ -6,14 +6,82 @@ from aether.protocol.protocol import AetherTransferClient
 from twisted.internet import reactor, threads
 from twisted.internet.protocol import ClientCreator
 
+import pybonjour
+import time
+import select
 
-def send(x):
-    reactor.callLater(1, x.sendFile, '/home/fin/Mixotic_200_-_Anonymous_-_200_9_hours.mp3')
+from optparse import OptionParser
+
+import sys
+from datetime import datetime, timedelta
+
+
+regtype = u'_at_nomin_aether._tcp'
+timeout = 5
+resolved = {}
+
+class Sender(object):
+    def __init__(self, filename):
+        self.filename = filename
+    def send(self, x):
+        reactor.callLater(1, x.sendFile, self.filename)
+
+def resolve_callback(serviceName, sdRef, flags, interfaceIndex, errorCode, fullname,
+                        hosttarget, port, txtRecord):
+    if errorCode:
+        print errorCode
+        return
+    resolved[serviceName] = (fullname, hosttarget, port, txtRecord)
+    print resolved[serviceName]
+
+
 
 if __name__ == '__main__':
-    c = ClientCreator(reactor, AetherTransferClient)
-   
-    x = c.connectTCP('localhost', 9999)
-    x.addCallback(send)
-    reactor.run()
+    do = sys.argv[1]
+    if do=='send':
+        target = sys.argv[2]
+        filename = sys.argv[3]
+        
+        c = ClientCreator(reactor, AetherTransferClient)
+       
+        x = c.connectTCP('localhost', 9999)
+        x.addCallback(Sender(filename).send)
+        reactor.run()
+
+
+    if do=='list':
+
+        def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
+                                    regtype, replyDomain):
+            if errorCode:
+                print 'what'
+                print errorCode
+                return
+            resolve_sdRef = pybonjour.DNSServiceResolve(0,
+                                                            interfaceIndex,
+                                                            serviceName,
+                                                            regtype,
+                                                            replyDomain,
+                                                            lambda *x, **y: resolve_callback(serviceName, *x, **y))
+            try:
+                while not serviceName in resolved:
+                    ready = select.select([resolve_sdRef], [], [])
+                    if resolve_sdRef not in ready[0]:
+                        print 'Resolve timed out'
+                        break
+                    pybonjour.DNSServiceProcessResult(resolve_sdRef)
+            finally:
+                resolve_sdRef.close()
+
+        browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype,
+                                                          callBack = browse_callback)
+
+        end = datetime.now() + timedelta(seconds=10)
+
+        while(datetime.now()<end):
+            ready = select.select([browse_sdRef], [], [], 10) 
+            if browse_sdRef in ready[0]:
+                pybonjour.DNSServiceProcessResult(browse_sdRef)
+            time.sleep(0.5)
+                    
 
--- aether/server/main.py
+++ aether/server/main.py
@@ -6,6 +6,11 @@ from aether.protocol.protocol import AetherTransferServerFactory
 from twisted.internet import reactor, threads
 
 import time
+import pybonjour
+
+regtype = u'_at_nomin_aether._tcp'
+name = 'fin@finkpad'
+port = 9999
 
 class Lol:
     def __init__(self):
@@ -20,9 +25,18 @@ class Lol:
             print (self.last_received, received)
 
 l = Lol()
+sdRef = None
 
 if __name__ == '__main__':
-    factory = AetherTransferServerFactory('/tmp', l.cb)
-    reactor.listenTCP(9999, factory)
-    reactor.run()
+    try:
+        factory = AetherTransferServerFactory('/tmp', l.cb)
+        reactor.listenTCP(port, factory)
+        sdRef = pybonjour.DNSServiceRegister(name = name,
+                                             regtype = regtype,
+                                             port = port,
+                                             callBack = lambda **x: x)
+        reactor.run()
+    finally:
+        if sdRef:
+            sdRef.close()
 
